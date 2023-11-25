557822f6a02a0ed8a62849124127004d2a75f104
Angus <angus.goody@gmail.com>
2023-11-25 22:41:30 +0000

    updated skill

--- src/collections/Skills.ts
+++ src/collections/Skills.ts
@@ -15,6 +15,45 @@ const Skills: CollectionConfig = {
         description: 'Name of this skill',
       },
     },
+    {
+      name: 'category',
+      label: 'Skill Category',
+      type: 'relationship',
+      relationTo: ['skill-categories'],
+      required: true,
+    },
+    {
+      type: 'tabs',
+      tabs: [
+        {
+          label: 'Light mode',
+          fields: [
+            {
+              name: 'logo',
+              label: 'Logo',
+              type: 'upload',
+              relationTo: 'media',
+              required: true,
+            },
+          ],
+        },
+        {
+          label: 'Dark mode',
+          fields: [
+            {
+              name: 'logo_dark_mode',
+              label: 'Logo for dark mode',
+              type: 'upload',
+              relationTo: 'media',
+              required: false,
+              admin: {
+                description: 'A logo that will be visible on a darker background',
+              },
+            },
+          ],
+        },
+      ],
+    },
   ],
 }
 

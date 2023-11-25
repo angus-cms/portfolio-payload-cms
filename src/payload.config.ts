c230e2d189d74088414b790a136015318b17b9f8
Angus <angus.goody@gmail.com>
2023-11-25 15:38:15 +0000

    Added SEO plugin

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,9 +5,17 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
+import seo from '@payloadcms/plugin-seo'
+import type { GenerateTitle } from '@payloadcms/plugin-seo/types'
 
 import Users from './collections/Users'
 import Pages from './collections/Pages'
+import { Media } from './collections/Media'
+
+
+const generateTitle: GenerateTitle = () => {
+  return 'My Website'
+}
 
 export default buildConfig({
   admin: {
@@ -15,14 +23,21 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: slateEditor({}),
-  collections: [Users, Pages],
+  collections: [Users, Pages, Media],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
-  plugins: [payloadCloud()],
+  plugins: [
+    seo({
+      collections: ['pages'],
+      generateTitle,
+      uploadsCollection: 'media',
+    }),
+    payloadCloud()
+    ],
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),

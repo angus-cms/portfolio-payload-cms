03f90e84bbdd7a9a4c1fdd713dd85970fb50979f
Angus <angus.goody@gmail.com>
2023-11-25 14:45:48 +0000

    Inital files

---
+++ src/collections/Pages.ts
@@ -0,0 +1,22 @@
+import { CollectionConfig } from 'payload/types'
+
+const Pages: CollectionConfig = {
+  slug: 'pages',
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'subtitle',
+      type: 'text',
+      required: true,
+    },
+  ],
+}
+
+export default Pages
